7fcb2258a7851db7354880a4d6358625bf96dcac
Hernan Finucci <hfinucci@itba.edu.ar>
2023-10-01 21:23:13 -0300

    🦘

--- Assets/Scripts/Controllers/JumpController.cs
+++ Assets/Scripts/Controllers/JumpController.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class JumpController : MonoBehaviour, IJumpable
 {
     public float JumpHeight => _jumpHeight;
-    private float _jumpHeight = 2f;
+    private float _jumpHeight = 3.5f;
     private float _jumpSpeed;
     private float gravity = -9.81f;
-    private float gravityScale = 1;
+    private float gravityScale = 1.2f;
     [SerializeField] public CharacterController _controller;
     [SerializeField] private KeyCode _jump = KeyCode.Space;
     // Start is called before the first frame update
     void Start()
     {
         _controller = GetComponent<CharacterController>();
+        _controller.minMoveDistance = 0;
     }
 
     // Update is called once per frame
@@ -24,8 +25,7 @@ public class JumpController : MonoBehaviour, IJumpable
     }
 
     public void Jump() {
-         if (Input.GetKeyDown(_jump))
-        {
+        if (Input.GetKeyDown(_jump) && _controller.isGrounded) {
             _jumpSpeed = Mathf.Sqrt(_jumpHeight * -2f * (gravity * gravityScale));
         }
         _jumpSpeed += gravity * gravityScale * Time.deltaTime;
