f0bad0c8242ceec97137b0bd9c89ad7c4db20272
arfernandez2000 <arfernandez@itba.edu.ar>
2023-10-04 23:46:22 -0300

    Added loading bar

--- Assets/Scripts/Managers/MenusManager.cs
+++ Assets/Scripts/Managers/MenusManager.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenusManager : MonoBehaviour
 {
+    public GameObject ui_load;
+    public Image loading_bar;
 
     public void Start() {
         Cursor.lockState = CursorLockMode.None;
@@ -16,10 +19,20 @@ public class MenusManager : MonoBehaviour
     }
 
     public void StartGame() {
-        SceneManager.LoadScene(1, LoadSceneMode.Single);
+        StartCoroutine("AsyncStart");
     }
 
     public void MainMenuLoad() {
         SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+
+    public IEnumerator AsyncStart() {
+        ui_load.SetActive(true);
+        AsyncOperation loading = SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+
+        while(!loading.isDone){
+            loading_bar.fillAmount = loading.progress;
+            yield return null;
+        }
     }
 }
